30024aeac39d53fa500e0641a549d8bea089b13a
Theodore DeRego <tderego94@gmail.com>
2016-08-22 15:12:43 -1000

    memory frame allocator

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ extern crate rlibc;
 extern crate spin;
 extern crate multiboot2;
 
+use memory::FrameAllocator;
+
+mod memory;
 #[macro_use]
 mod vga_buffer;
 
@@ -24,6 +27,33 @@ pub extern fn rust_main(multiboot_info_addr: usize) {
     for area in memory_map_tag.memory_areas() {
         println!("    start: 0x{:x}, length: 0x{:x}",
                  area.base_addr, area.length);
+    }
+
+    let elf_sections_tag = boot_info.elf_sections_tag()
+                                    .expect("Elf-sections tag required");
+
+    println!("kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+                 section.addr, section.size, section.flags);
+    }
+
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+                                       .min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
+                                     .max().unwrap();
+    let multiboot_start = multiboot_info_addr;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    let mut frame_allocator =
+        memory::AreaFrameAllocator::new(kernel_start as usize, kernel_end as usize, multiboot_start,
+                                        multiboot_end, memory_map_tag.memory_areas());
+
+    for i in 0.. {
+        if let None = frame_allocator.allocate_frame() {
+            println!("allocated {} frames", i);
+            break;
+        }
     }
     
     loop { }
